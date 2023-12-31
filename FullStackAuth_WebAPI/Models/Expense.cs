ad57c63e71770cc5d8a56d01f55e5172c4b436bd
a2thedev <anthonygreen048@gmail.com>
2023-12-31 16:19:54 -0500

    I forgot to hit save on both Expense and Income models I created

--- FullStackAuth_WebAPI/Models/Expense.cs
+++ FullStackAuth_WebAPI/Models/Expense.cs
@@ -1,6 +1,28 @@
-﻿namespace FullStackAuth_WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
 {
-    public class Class
+    public class Expense
     {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public int Rating { get; set; }
+
+        public bool IsPaid { get; set; }
+
+        [ForeignKey("Budgeter")]
+
+        public string BudgeterId { get; set; }
+
+        public User Budgeter { get; set; }
     }
 }
--- FullStackAuth_WebAPI/Models/Income.cs
+++ FullStackAuth_WebAPI/Models/Income.cs
@@ -1,6 +1,24 @@
-﻿namespace FullStackAuth_WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
 {
     public class Income
     {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        [ForeignKey("Budgeter")]
+
+        public string BudgeterId { get; set; }
+
+        public User Budgeter { get; set; }
     }
 }
